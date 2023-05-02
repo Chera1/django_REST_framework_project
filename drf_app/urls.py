c887c470c20d805409a8ebd71084b34cc2729787
Chera <chera1904@mail.ru>
2023-05-02 16:13:25 +0600

    Deleting uncorrected url; Modified FilmAPIView for method post and get.

--- drf_app/urls.py
+++ drf_app/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('api/films/', views.FilmAPIView.as_view()),
-    # path('', views.main_page, name='main_page'),
 ]
--- drf_app/views.py
+++ drf_app/views.py
@@ -1,10 +1,25 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from drf_app.models import Film
 from drf_app.serializers import FilmSerializer
 
 
-class FilmAPIView(generics.ListAPIView):
-    queryset = Film.objects.all()
-    serializer_class = FilmSerializer
+class FilmAPIView(APIView):
+    def get(self, request):
+        queryset = Film.objects.all()
+        serializer_for_queryset = FilmSerializer(
+            instance=queryset,
+            many=True
+        )
+        return Response({"films_list": serializer_for_queryset.data})
+
+    def post(self, request):
+        new_film = Film.objects.create(
+            name=request.data['name'],
+            type=request.data['type']
+        )
+        return Response({'film': model_to_dict(new_film)})
