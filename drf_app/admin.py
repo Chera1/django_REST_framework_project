9afa6200095ef8874210cb49dd1326027b74afac
Chera <chera1904@mail.ru>
2023-05-02 13:25:32 +0600

    Creating app models; Creating djangoRESTFramework as app; Creating serializer for models; Creating urls for our API.

--- drf_app/admin.py
+++ drf_app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Actor, Film, ActorFilmWork
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    # Отображение полей в списке
+    list_display = ('full_name',)
+
+    # Поиск по полям
+    search_fields = ('full_name', 'id')
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = ActorFilmWork
+    autocomplete_fields = ['actor', ]
+
+
+@admin.register(Film)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (PersonFilmWorkInline,)
+
+    # Отображение полей в списке
+    list_display = ('name', 'type')
+
+    # Фильтрация в списке
+    list_filter = ('type',)
+
+    # Поиск по полям
+    search_fields = ('title', 'id')
