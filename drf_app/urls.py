a993b23af40458b0eca4d7e5cff7df6eb9d4a241
Chera <chera1904@mail.ru>
2023-05-03 19:19:12 +0600

    Adding second method initializing; Adding new classes of view for films and actors; Adding urls for lists and detail info about objects; Adding rest framework to settings.

--- drf_app/urls.py
+++ drf_app/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/films/', views.FilmAPIView.as_view()),
-    path('api/actors/', views.ActorAPIView.as_view()),
-    path('api/films/<int:pk>', views.FilmAPIView.as_view()),
-    path('api/actors/<int:pk>', views.ActorAPIView.as_view()),
+    path('api/films/', views.FilmAPIList.as_view()),
+    path('api/actors/', views.ActorAPIList.as_view()),
+    path('api/films/<int:pk>', views.FilmAPIUpdate.as_view()),
+    path('api/filmsdetail/<int:pk>', views.FilmAPIDetailView.as_view()),
+    path('api/actorsdetail/<int:pk>', views.ActorAPIDetailView.as_view()),
+    path('api/actors/<int:pk>', views.ActorAPIUpdate.as_view()),
 ]
--- drf_app/views.py
+++ drf_app/views.py
@@ -1,94 +1,34 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
-from drf_app.models import Film, Actor, ActorFilmWork
-from drf_app.serializers import FilmSerializer, ActorSerializer, ActorFilmWorkSerializer
-
-
-class FilmAPIView(APIView):
-    def get(self, request):
-        queryset = Film.objects.all()
-        serializer_for_queryset = FilmSerializer(
-            instance=queryset,
-            many=True
-        )
-        return Response({"films_list": serializer_for_queryset.data})
-
-    def post(self, request):
-        serializer_film = FilmSerializer(data=request.data)
-        serializer_film.is_valid(raise_exception=True)
-        serializer_film.save()
-
-        return Response({'film': serializer_film.data})
-
-    def put(self, request, *arg, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-
-        try:
-            instance = Film.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Objects does not exists'})
-
-        serializer = FilmSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'film': serializer.data})
-
-    def delete(self, request, *arg, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method DELETE not allowed'})
-
-        try:
-            instance = Film.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Objects does not exists'})
-
-        instance.delete()
-        return Response({'film': "deleted film " + str(pk)})
+from drf_app.models import Film, Actor
+from drf_app.serializers_2 import FilmSerializer, ActorSerializer
 
 
-class ActorAPIView(APIView):
-    def get(self, request):
-        queryset = Actor.objects.all()
-        serializer_for_queryset = ActorSerializer(
-            instance=queryset,
-            many=True
-        )
-        return Response({"actors_list": serializer_for_queryset.data})
+class FilmAPIList(generics.ListCreateAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
-    def post(self, request):
-        serializer_actor = ActorSerializer(data=request.data)
-        serializer_actor.is_valid(raise_exception=True)
-        serializer_actor.save()
 
-        return Response({'actor': serializer_actor.data})
+class FilmAPIUpdate(generics.UpdateAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
-    def put(self, request, *arg, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-        try:
-            instance = Actor.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Objects does not exists'})
 
-        serializer = ActorSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'actor': serializer.data})
+class FilmAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
-    def delete(self, request, *arg, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method DELETE not allowed'})
 
-        try:
-            instance = Actor.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Objects does not exists'})
+class ActorAPIList(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
 
-        instance.delete()
-        return Response({'actor': "deleted actor " + str(pk)})
+
+class ActorAPIUpdate(generics.UpdateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class ActorAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
