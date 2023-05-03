c32f209d17e285614a3deca010a84f1a3775cfaf
Chera <chera1904@mail.ru>
2023-05-03 11:52:05 +0600

    Adding urls for updating and deleting objects; Divided APIViews for films and actors and added methods of CRUD; Adding methods of serializers for updating and creating objects.

--- drf_app/serializers.py
+++ drf_app/serializers.py
@@ -1,15 +1,35 @@
 from rest_framework import serializers
 
+from drf_app.models import Film, Actor, ActorFilmWork
+
 
 class ActorSerializer(serializers.Serializer):
     full_name = serializers.CharField(max_length=255)
     age = serializers.IntegerField(allow_null=True)
 
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.full_name = validated_data.get('full_name', instance.full_name)
+        instance.age = validated_data.get('age', instance.age)
+        instance.save()
+        return instance
+
 
 class FilmSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     type = serializers.CharField(max_length=100)
-    actors = ActorSerializer(many=True)
+    # actors = ActorSerializer(many=True, default=True)
+
+    def create(self, validated_data):
+        return Film.objects.create(name=validated_data['name'], type=validated_data['type'])
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.type = validated_data.get('type', instance.type)
+        instance.save()
+        return instance
 
 
 class ActorFilmWorkSerializer(serializers.Serializer):
